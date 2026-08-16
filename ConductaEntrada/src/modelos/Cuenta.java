f93690a696a57351759fd3b97bdb1da51710f7ee
X-Rj-09-X <rikardojfp@hotmail.com>
2026-08-15 20:29:26 -0500

    creacion de clase cuenta

--- ConductaEntrada/src/modelos/Cuenta.java
+++ ConductaEntrada/src/modelos/Cuenta.java
@@ -9,5 +9,43 @@ package modelos;
  * @author Ricardo J
  */
 public class Cuenta {
+    private String numeroCuenta;
+    private long dniCliente;
+    private double saldoActual;
+    
+    public Cuenta(){
+    }
+
+    public Cuenta(String numeroCuenta, long dniCliente, double saldoActual) {
+        this.numeroCuenta = numeroCuenta;
+        this.dniCliente = dniCliente;
+        this.saldoActual = saldoActual;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(long dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+    
+    
     
 }
