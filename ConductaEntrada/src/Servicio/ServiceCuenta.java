b9c902c383ff93bbd7a063cd863da59fd16c3b00
X-Rj-09-X <rikardojfp@hotmail.com>
2026-08-16 19:28:04 -0500

    Implementacion de ServiceCuenta

---
+++ ConductaEntrada/src/Servicio/ServiceCuenta.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelos.Cuenta;
+
+/**
+ *
+ * @author Ricardo J
+ */
+public class ServiceCuenta implements IServiceCuenta {
+    
+    private List<Cuenta> cuentas;
+    
+    public ServiceCuenta(){
+        cuentas = new ArrayList();
+    }
+
+    @Override
+    public List<Cuenta> ListarCuenta() {
+        return cuentas;
+    }
+
+    @Override
+    public Cuenta obtenerCuenta(String numeroCuenta) {
+        for (Cuenta cuenta : cuentas) {
+            
+            if(cuenta.getNumeroCuenta().equals(numeroCuenta)){
+                return cuenta;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void crearCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
+    
+    
+    
+}
